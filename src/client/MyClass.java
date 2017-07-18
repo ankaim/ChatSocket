7e5f3e72492297699d4dc5d856f7098e0e06783e
ankaim <kaim@mail.ru>
2017-07-18 17:34:12 +0200

    2

--- src/client/MyClass.java
+++ src/client/MyClass.java
@@ -1,3 +1,35 @@
+package client;
 
-public class Client {
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class MyClass {
+    static Socket socket = null;
+
+    public static void main(String[] args) {
+        try {
+            socket = new Socket("localhost", 8189);
+            DataInputStream in = new DataInputStream(socket.getInputStream());
+            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+            Scanner scn = new Scanner(System.in);
+
+            while(true){
+                String str = scn.nextLine();
+                if(str.equals("end")) break;
+                out.writeUTF(str);
+                System.out.println("from server: "+in.readUTF());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            try {
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
