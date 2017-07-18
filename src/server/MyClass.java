ec39c20e136c741b5664218f213abcd623ee27aa
ankaim <kaim@mail.ru>
2017-07-18 17:12:54 +0200

    Organization package

--- src/server/MyClass.java
+++ src/server/MyClass.java
@@ -5,24 +5,31 @@ import java.net.Socket;
 import java.util.Scanner;
 
 public class Sever {
+    static ServerSocket serverSocket = null;
+    static Socket socket = null;
+
     public static void main(String[] args) {
         try {
-            ServerSocket serverSocket = new ServerSocket(8189);
+            serverSocket = new ServerSocket(8189);
             System.out.println("Сервер ждет подключения...");
-            Socket socket = serverSocket.accept();
+            socket = serverSocket.accept();
             System.out.println("Клиет подключен!");
             Scanner scn = new Scanner(socket.getInputStream());
             PrintWriter pw = new PrintWriter(socket.getOutputStream());
-            while (true){
+            while (true) {
                 String str = scn.nextLine();
-                if(str.equals("end")) break;
-                pw.write("echo "+str);
+                if (str.equals("end")) break;
+                pw.write("echo " + str);
                 pw.flush();
             }
-
-
         } catch (IOException e) {
             e.printStackTrace();
+        } finally {
+            try {
+                serverSocket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
